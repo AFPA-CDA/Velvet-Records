367fe84e0087161dcab6423fdb1c4e59d2ce6ad4
Julien-Broyard <broyard.dev@gmail.com>
2020-03-03 11:10:20 +0100

    :fire: Refactor db connection

--- models/connection.php
+++ models/connection.php
@@ -3,6 +3,21 @@
 // Creates a Singleton class that helps retrieving the Database connection
 class Database
 {
+    const DB_HOST = "localhost";
+    const DB_NAME = "record";
+    const DB_USER = "root";
+    const DB_PASSWORD = "root";
+    const DB_OPTIONS = [
+        // Leave column names as returned by the database driver
+        PDO::ATTR_CASE => PDO::CASE_NATURAL,
+        // Throws exception when an error occurs
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        // Empty string are converted to NULL
+        PDO::ATTR_ORACLE_NULLS => PDO::NULL_EMPTY_STRING,
+        // Sets the default fetch mode to FETCH_OBJ
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ
+    ];
+
     protected static $PDOInstance;
 
     /**
@@ -14,25 +29,14 @@ class Database
         if (!isset(self::$PDOInstance)) {
             // The try catch block is used to run code and catch the errors uf there are any
             try {
-                // Array of the used options for the PDO instance
-                $options = [
-                    // Leave column names as returned by the database driver
-                    PDO::ATTR_CASE => PDO::CASE_NATURAL,
-                    // Throws exception when an error occurs
-                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-                    // Empty string are converted to NULL
-                    PDO::ATTR_ORACLE_NULLS => PDO::NULL_EMPTY_STRING,
-                    // Sets the default fetch mode to FETCH_OBJ
-                    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ
-                ];
-
+                // The PDO connection string
+                $connectionStr = sprintf("mysql:host=%s;dbname=%s;charset=utf8", self::DB_HOST, self::DB_NAME);
                 // Creates an instance of the PDO object
-                self::$PDOInstance = new PDO("mysql:host=localhost;dbname=record;charset=utf8", "root", "root", $options);
+                self::$PDOInstance = new PDO($connectionStr, self::DB_USER, self::DB_PASSWORD, self::DB_OPTIONS);
             } catch (PDOException $e) {
                 die($e->getMessage());
             }
         }
-
         // Otherwise it returns the already created instance
         return self::$PDOInstance;
     }
